f4d7b8b8bd26999c4a9d9496217f37b75031a2a1
Kholid <68140053+hp-alex@users.noreply.github.com>
2021-11-28 11:57:45 +0500

    App updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import store from './redux/store'
 import {Provider} from 'react-redux'
 import Home from './container'
+// App updated by Makhmudjon
 
 function App() {
   return (
